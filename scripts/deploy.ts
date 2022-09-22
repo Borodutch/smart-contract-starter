51a5b090110d5f3d89e57f0d36a07cb2a5b4011f
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-09-22 09:43:23 -0700

    add utils.formatEther

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,15 @@
 import { ethers, run } from 'hardhat'
+import { utils } from 'ethers'
 
 async function main() {
   const [deployer] = await ethers.getSigners()
 
   // Deploy the contract
   console.log('Deploying contracts with the account:', deployer.address)
-  console.log('Account balance:', (await deployer.getBalance()).toString())
+  console.log(
+    'Account balance:',
+    utils.formatEther(await deployer.getBalance())
+  )
 
   const provider = ethers.provider
   const { chainId } = await provider.getNetwork()
@@ -23,8 +27,14 @@ async function main() {
   const Contract = await ethers.getContractFactory(contractName)
   const contract = await Contract.deploy(contractName, contractSymbol)
 
-  console.log('Deploy tx gas price:', contract.deployTransaction.gasPrice)
-  console.log('Deploy tx gas limit:', contract.deployTransaction.gasLimit)
+  console.log(
+    'Deploy tx gas price:',
+    utils.formatEther(contract.deployTransaction.gasPrice || 0)
+  )
+  console.log(
+    'Deploy tx gas limit:',
+    utils.formatEther(contract.deployTransaction.gasLimit)
+  )
   await contract.deployed()
   const address = contract.address
 
