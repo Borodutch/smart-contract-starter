55d65e38bdf64be892c408d43995a06ae01b481b
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-12-16 16:21:03 -0800

    Update dependencies and Solidity version

--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -1,38 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyERC721 is ERC721, Ownable {
-  using Counters for Counters.Counter;
-
-  Counters.Counter public tokenId;
+  uint public tokenId;
 
   constructor(
-    string memory tokenName,
-    string memory tokenSymbol
-  ) ERC721(tokenName, tokenSymbol) {}
+    string memory name,
+    string memory symbol,
+    address initialOwner
+  ) ERC721(name, symbol) Ownable(initialOwner) {}
 
   function mint() external {
-    uint256 _tokenId = tokenId.current();
+    uint256 _tokenId = tokenId;
     _safeMint(msg.sender, _tokenId);
-    tokenId.increment();
-  }
-
-  function _beforeTokenTransfer(
-    address _from,
-    address _to,
-    uint256 _tokenId,
-    uint256 _batchSize
-  ) internal override(ERC721) {
-    super._beforeTokenTransfer(_from, _to, _tokenId, _batchSize);
-  }
-
-  function supportsInterface(
-    bytes4 _interfaceId
-  ) public view override(ERC721) returns (bool) {
-    return super.supportsInterface(_interfaceId);
+    tokenId++;
   }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.19',
+    version: '0.8.23',
     settings: {
       optimizer: {
         enabled: true,
